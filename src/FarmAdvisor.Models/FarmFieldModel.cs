ae3b6541e6612e7c161d26be89a2190bc204f319
Estifanos Neway <estifanos.neway.d@gmail.com>
2023-01-24 18:13:18 +0300

    implemented 'create farm'

--- src/FarmAdvisor.Models/FarmFieldModel.cs
+++ src/FarmAdvisor.Models/FarmFieldModel.cs
@@ -4,9 +4,8 @@ namespace FarmAdvisor.Models
     {
         public Guid FieldId { get; set; }
         public Guid FarmId { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
         public decimal Altitude { get; set; }
-        public string Polygon { get; set; }
-
+        public string Polygon { get; set; }  = null!;
     }
 }
