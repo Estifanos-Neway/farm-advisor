50446ff39a0f354ef9851ef33ad43eec690d2bbd
Estifanos Neway <Estifanos.Neway.D@gmail.com>
2023-02-14 00:21:42 +0300

    small fixes

--- src/FarmAdvisor.Controllers/FieldsController.cs
+++ src/FarmAdvisor.Controllers/FieldsController.cs
@@ -23,96 +23,57 @@ namespace FarmAdvisor.Controllers
         [HttpPost]
         public IActionResult postField([FromBody] Field field)
         {
-            try
-            {
-                Guid? userId = jwtAuthenticationController.getCurrentUserId(HttpContext);
-                Farm? farm = farmDataAccess.getByUserAndFarmId((Guid)userId!, field.FarmId);
-                if (farm == null)
-                    return NotFound("Farm_Not_Found");
-                field.UserId = userId;
-                field = fieldDataAccess.add(field);
-                return Ok(field);
-            }
-            catch (Exception e)
-            {
-                Console.Write(e);
-                return StatusCode(500);
-            }
+            Guid? userId = jwtAuthenticationController.getCurrentUserId(HttpContext);
+            Farm? farm = farmDataAccess.getByUserAndFarmId((Guid)userId!, field.FarmId);
+            if (farm == null)
+                return NotFound("Farm_Not_Found");
+            field.UserId = userId;
+            field = fieldDataAccess.add(field);
+            return Ok(field);
         }
         [HttpGet]
         [Route("{fieldId?}")]
         public IActionResult getField(Guid fieldId)
         {
-            try
+            Guid? userId = jwtAuthenticationController.getCurrentUserId(HttpContext);
+            Field? field = fieldDataAccess.getByUserAndFieldId((Guid)userId!, fieldId);
+            if (field == null)
             {
-                Guid? userId = jwtAuthenticationController.getCurrentUserId(HttpContext);
-                Field? field = fieldDataAccess.getByUserAndFieldId((Guid)userId!, fieldId);
-                if (field == null)
-                {
-                    return NotFound("Field_Not_Found");
-                }
-                return Ok(field);
+                return NotFound("Field_Not_Found");
             }
-            catch (Exception e)
-            {
-                Console.Write(e);
-                return StatusCode(500);
-            }
+            return Ok(field);
+
         }
         [HttpPatch]
         [Route("{fieldId?}")]
         public IActionResult patchField(Guid fieldId, FieldUpdate fieldUpdates)
         {
-            try
+            Guid? userId = jwtAuthenticationController.getCurrentUserId(HttpContext);
+            Field? field = fieldDataAccess.updateByUserAndFieldId((Guid)userId!, fieldId, fieldUpdates);
+            if (field == null)
             {
-                Guid? userId = jwtAuthenticationController.getCurrentUserId(HttpContext);
-                Field? field = fieldDataAccess.updateByUserAndFieldId((Guid)userId!, fieldId, fieldUpdates);
-                if (field == null)
-                {
-                    return NotFound("Field_Not_Found");
-                }
-                return Ok(field);
+                return NotFound("Field_Not_Found");
             }
-            catch (Exception e)
-            {
-                Console.Write(e);
-                return StatusCode(500);
-            }
+            return Ok(field);
         }
         [HttpDelete]
         [Route("{fieldId?}")]
         public IActionResult deleteField(Guid fieldId)
         {
-            try
+            Guid? userId = jwtAuthenticationController.getCurrentUserId(HttpContext);
+            Field? field = fieldDataAccess.deleteByUserAndFieldId((Guid)userId!, fieldId);
+            if (field == null)
             {
-                Guid? userId = jwtAuthenticationController.getCurrentUserId(HttpContext);
-                Field? field = fieldDataAccess.deleteByUserAndFieldId((Guid)userId!, fieldId);
-                if (field == null)
-                {
-                    return NotFound("Field_Not_Found");
-                }
-                return Ok(field);
+                return NotFound("Field_Not_Found");
             }
-            catch (Exception e)
-            {
-                Console.Write(e);
-                return StatusCode(500);
-            }
+            return Ok(field);
         }
         [HttpGet]
         [Route("{fieldId?}/sensors")]
         public IActionResult getSensors(Guid fieldId)
         {
-            try
-            {
-                Sensor[]? sensors = sensorDataAccess.getByFieldId(fieldId);
-                return Ok(sensors);
-            }
-            catch (Exception e)
-            {
-                Console.Write(e);
-                return StatusCode(500);
-            }
+            Sensor[]? sensors = sensorDataAccess.getByFieldId(fieldId);
+            return Ok(sensors);
         }
     }
 }
--- src/FarmAdvisor.Controllers/SensorsController .cs
+++ src/FarmAdvisor.Controllers/SensorsController .cs
@@ -24,157 +24,116 @@ namespace FarmAdvisor.Controllers
         [HttpPost]
         public async Task<IActionResult> postSensorAsync([FromBody] SensorInput sensorInput)
         {
-            try
+            if (!Utils.isValidLatitude(sensorInput.Lat))
+                return BadRequest("Invalid_Latitude");
+            if (!Utils.isValidLongitude(sensorInput.Long))
+                return BadRequest("Invalid_Longitude");
+            Guid? userId = jwtAuthenticationController.getCurrentUserId(HttpContext);
+            Field? field = fieldDataAccess.getByUserAndFieldId((Guid)userId!, sensorInput.FieldId);
+            if (field == null)
+                return NotFound("Field_Not_Found");
+            Sensor sensor = new Sensor()
             {
-                if (!Utils.isValidLatitude(sensorInput.Lat))
-                    return BadRequest("Invalid_Latitude");
-                if (!Utils.isValidLongitude(sensorInput.Long))
-                    return BadRequest("Invalid_Longitude");
-                Guid? userId = jwtAuthenticationController.getCurrentUserId(HttpContext);
-                Field? field = fieldDataAccess.getByUserAndFieldId((Guid)userId!, sensorInput.FieldId);
-                if (field == null)
-                    return NotFound("Field_Not_Found");
-                Sensor sensor = new Sensor()
-                {
-                    FieldId = sensorInput.FieldId,
-                    SerialNo = sensorInput.SerialNo,
-                    GDD = sensorInput.GDD,
-                    DefaultGDD = sensorInput.DefaultGDD,
-                    Long = sensorInput.Long,
-                    Lat = sensorInput.Lat,
-                    InstallationDate = sensorInput.InstallationDate,
-                    LastCuttingDate = sensorInput.LastCuttingDate,
-                    BaseTemperature = sensorInput.BaseTemperature,
-                    UserId = userId
-                };
-                sensorDataAccess.add(sensor);
-                DateTime date = DateTime.UtcNow.Date;
-                Guid SensorId = (Guid)sensor.SensorId!;
-                List<SensorStatistic> sensorStatistics = new List<SensorStatistic>();
-                List<OneDayWeatherForecast> forecasts = await weatherForecastService.getForecastAsync(field.Altitude, sensor.Lat, sensor.Long, sensor.BaseTemperature, sensor.GDD);
-                List<double> temps = new List<double>();
-                List<double> gdds = new List<double>();
-                double lastGdd = forecasts[0].GDD;
-                bool cuttingDateSet = false;
-                double defaultGDD = sensor.DefaultGDD;
-                foreach (OneDayWeatherForecast forecast in forecasts)
-                {
-                    sensorStatistics.Add(new SensorStatistic()
-                    {
-                        Date = date,
-                        SensorId = SensorId,
-                        averageTemperature = forecast.averageTemperature,
-                        GDD = forecast.GDD
-                    });
-                    if (!cuttingDateSet && forecast.GDD > defaultGDD)
-                    {
-                        double downDt = defaultGDD - lastGdd;
-                        double upDt = forecast.GDD - defaultGDD;
-                        sensor.cuttingDate = downDt > upDt ? date : date.AddDays(-1);
-                        cuttingDateSet = true;
-                    }
-                    lastGdd = forecast.GDD;
-                    temps.Add(forecast.averageTemperature);
-                    gdds.Add(forecast.GDD);
-                    date = date.AddDays(1);
-                }
-                double[] tempIncPattern = Utils.getIncPattern(temps);
-                double[] gddIncPattern = Utils.getIncPattern(gdds);
-                int incPatternLength = tempIncPattern.Length;
-                double lastTemp = temps[^1];
-                int loopCount = 100 - sensorStatistics.Count;
-                for (int dateCount = 0; dateCount < loopCount; dateCount++)
-                {
-                    lastTemp += tempIncPattern[dateCount % incPatternLength];
-                    lastGdd += gddIncPattern[dateCount % incPatternLength];
-                    sensorStatistics.Add(new SensorStatistic()
-                    {
-                        Date = date,
-                        SensorId = SensorId,
-                        averageTemperature = lastTemp,
-                        GDD = lastGdd
-                    });
-                    if (!cuttingDateSet && lastGdd > defaultGDD)
-                    {
-                        double downDt = defaultGDD - lastGdd;
-                        double upDt = lastGdd - defaultGDD;
-                        sensor.cuttingDate = downDt > upDt ? date : date.AddDays(-1);
-                        cuttingDateSet = true;
-                    }
-                    date = date.AddDays(1);
-                }
-                sensorStatisticsDataAccess.addMany(sensorStatistics);
-                return Ok(sensor);
-            }
-            catch (Exception e)
+                FieldId = sensorInput.FieldId,
+                SerialNo = sensorInput.SerialNo,
+                GDD = sensorInput.GDD,
+                DefaultGDD = sensorInput.DefaultGDD,
+                Long = sensorInput.Long,
+                Lat = sensorInput.Lat,
+                InstallationDate = sensorInput.InstallationDate,
+                LastCuttingDate = sensorInput.LastCuttingDate,
+                BaseTemperature = sensorInput.BaseTemperature,
+                UserId = userId
+            };
+            sensorDataAccess.add(sensor);
+            DateTime date = DateTime.UtcNow.Date;
+            Guid SensorId = (Guid)sensor.SensorId!;
+            List<SensorStatistic> sensorStatistics = new List<SensorStatistic>();
+            List<OneDayWeatherForecast> forecasts = await weatherForecastService.getForecastAsync(field.Altitude, sensor.Lat, sensor.Long, sensor.BaseTemperature, sensor.GDD);
+            List<double> temps = new List<double>();
+            List<double> gdds = new List<double>();
+            double lastGdd = forecasts[0].GDD;
+            bool cuttingDateSet = false;
+            double defaultGDD = sensor.DefaultGDD;
+            foreach (OneDayWeatherForecast forecast in forecasts)
             {
-                Console.Write(e);
-                return StatusCode(500);
+                sensorStatistics.Add(new SensorStatistic()
+                {
+                    Date = date,
+                    SensorId = SensorId,
+                    averageTemperature = forecast.averageTemperature,
+                    GDD = forecast.GDD
+                });
+                if (!cuttingDateSet && forecast.GDD > defaultGDD)
+                {
+                    double downDt = defaultGDD - lastGdd;
+                    double upDt = forecast.GDD - defaultGDD;
+                    sensor.cuttingDate = downDt > upDt ? date : date.AddDays(-1);
+                    cuttingDateSet = true;
+                }
+                lastGdd = forecast.GDD;
+                temps.Add(forecast.averageTemperature);
+                gdds.Add(forecast.GDD);
+                date = date.AddDays(1);
             }
+            double[] tempIncPattern = Utils.getIncPattern(temps);
+            double[] gddIncPattern = Utils.getIncPattern(gdds);
+            int incPatternLength = tempIncPattern.Length;
+            double lastTemp = temps[^1];
+            int loopCount = 100 - sensorStatistics.Count;
+            for (int dateCount = 0; dateCount < loopCount; dateCount++)
+            {
+                lastTemp += tempIncPattern[dateCount % incPatternLength];
+                lastGdd += gddIncPattern[dateCount % incPatternLength];
+                sensorStatistics.Add(new SensorStatistic()
+                {
+                    Date = date,
+                    SensorId = SensorId,
+                    averageTemperature = lastTemp,
+                    GDD = lastGdd
+                });
+                if (!cuttingDateSet && lastGdd > defaultGDD)
+                {
+                    double downDt = defaultGDD - lastGdd;
+                    double upDt = lastGdd - defaultGDD;
+                    sensor.cuttingDate = downDt > upDt ? date : date.AddDays(-1);
+                    cuttingDateSet = true;
+                }
+                date = date.AddDays(1);
+            }
+            sensorStatisticsDataAccess.addMany(sensorStatistics);
+            return Ok(sensor);
         }
         [HttpGet]
         [Route("{sensorId?}")]
         public IActionResult getSensor(Guid sensorId)
         {
-            try
-            {
-                Guid? userId = jwtAuthenticationController.getCurrentUserId(HttpContext);
-                Sensor? sensor = sensorDataAccess.getByUserAndSensorId((Guid)userId!, sensorId);
-                return Ok(sensor);
-            }
-            catch (Exception e)
-            {
-                Console.Write(e);
-                return StatusCode(500);
-            }
+            Guid? userId = jwtAuthenticationController.getCurrentUserId(HttpContext);
+            Sensor? sensor = sensorDataAccess.getByUserAndSensorId((Guid)userId!, sensorId);
+            return Ok(sensor);
         }
         [HttpPatch]
         [Route("{sensorId?}")]
         public IActionResult patchSensor(Guid sensorId, SensorUpdate sensorUpdates)
         {
-            try
-            {
-                Guid? userId = jwtAuthenticationController.getCurrentUserId(HttpContext);
-                Sensor? sensor = sensorDataAccess.updateByUserAndSensorId((Guid)userId!, sensorId, sensorUpdates);
-                return Ok(sensor);
-            }
-            catch (Exception e)
-            {
-                Console.Write(e);
-                return StatusCode(500);
-            }
+            Guid? userId = jwtAuthenticationController.getCurrentUserId(HttpContext);
+            Sensor? sensor = sensorDataAccess.updateByUserAndSensorId((Guid)userId!, sensorId, sensorUpdates);
+            return Ok(sensor);
         }
         [HttpDelete]
         [Route("{sensorId?}")]
         public IActionResult deleteSensor(Guid sensorId)
         {
-            try
-            {
-                Guid? userId = jwtAuthenticationController.getCurrentUserId(HttpContext);
-                Sensor? sensor = sensorDataAccess.deleteByUserAndSensorId((Guid)userId!, sensorId);
-                return Ok(sensor);
-            }
-            catch (Exception e)
-            {
-                Console.Write(e);
-                return StatusCode(500);
-            }
+            Guid? userId = jwtAuthenticationController.getCurrentUserId(HttpContext);
+            Sensor? sensor = sensorDataAccess.deleteByUserAndSensorId((Guid)userId!, sensorId);
+            return Ok(sensor);
         }
         [HttpGet]
         [Route("{sensorId?}/statistics")]
         public IActionResult getStatistics(Guid sensorId)
         {
-            try
-            {
-                Guid? userId = jwtAuthenticationController.getCurrentUserId(HttpContext);
-                SensorStatistic[] sensorStatistic = sensorStatisticsDataAccess.getBySensorId(sensorId)!;
-                return Ok(sensorStatistic);
-            }
-            catch (Exception e)
-            {
-                Console.Write(e);
-                return StatusCode(500);
-            }
+            SensorStatistic[] sensorStatistic = sensorStatisticsDataAccess.getBySensorId(sensorId)!;
+            return Ok(sensorStatistic);
         }
         [HttpPost]
         [Route("{sensorId?}/gdd-resets")]
@@ -205,43 +164,27 @@ namespace FarmAdvisor.Controllers
         [Route("{sensorId?}/gdd-resets")]
         public IActionResult getGddResets(Guid sensorId)
         {
-            try
-            {
-                Guid? userId = jwtAuthenticationController.getCurrentUserId(HttpContext);
-                Sensor? sensor = sensorDataAccess.getByUserAndSensorId((Guid)userId!, sensorId);
-                SensorGddReset[] sensorGddResets = sensorGddResetDataAccess.getBySensorId((Guid)sensor!.SensorId!);
-                return Ok(sensorGddResets);
-            }
-            catch (Exception e)
-            {
-                Console.Write(e);
-                return StatusCode(500);
-            }
+            Guid? userId = jwtAuthenticationController.getCurrentUserId(HttpContext);
+            Sensor? sensor = sensorDataAccess.getByUserAndSensorId((Guid)userId!, sensorId);
+            SensorGddReset[] sensorGddResets = sensorGddResetDataAccess.getBySensorId((Guid)sensor!.SensorId!);
+            return Ok(sensorGddResets);
         }
         [HttpPost]
         [Route("{sensorId?}/trigger")]
         public IActionResult triggerSensor(Guid sensorId)
         {
+            Guid? userId = jwtAuthenticationController.getCurrentUserId(HttpContext);
+            Sensor? sensor = sensorDataAccess.getByUserAndSensorId((Guid)userId!, sensorId);
             try
             {
-                Guid? userId = jwtAuthenticationController.getCurrentUserId(HttpContext);
-                Sensor? sensor = sensorDataAccess.getByUserAndSensorId((Guid)userId!, sensorId);
-                try
-                {
-                    SensorStatistic sensorStatistic = sensorStatisticsDataAccess.getBySensorIdAndDate(sensorId, DateTime.UtcNow.Date)!;
-                    sensor = sensorDataAccess.updateByUserAndSensorId((Guid)userId!, sensorId, new SensorUpdate() { GDD = sensorStatistic.GDD });
-                }
-                catch (Exception e)
-                {
-                    Console.Write(e);
-                }
-                return Ok(sensor);
+                SensorStatistic sensorStatistic = sensorStatisticsDataAccess.getBySensorIdAndDate(sensorId, DateTime.UtcNow.Date)!;
+                sensor = sensorDataAccess.updateByUserAndSensorId((Guid)userId!, sensorId, new SensorUpdate() { GDD = sensorStatistic.GDD });
             }
             catch (Exception e)
             {
                 Console.Write(e);
-                return StatusCode(500);
             }
+            return Ok(sensor);
         }
     }
 }
